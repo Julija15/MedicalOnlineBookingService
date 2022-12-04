8862833970667e0f0056f9ed8a5416a88301ecd9
Julija15 <1771808@mail.ru>
2022-12-04 20:58:40 +0300

    Initial commit

---
+++ src/main/java/com/example/medicalonlinebookingservice/entity/User.java
@@ -0,0 +1,23 @@
+package com.example.medicalonlinebookingservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+    private String username;
+    private String password;
+    private String email;
+}
---
+++ src/test/java/com/example/medicalonlinebookingservice/MedicalOnlineBookingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medicalonlinebookingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalOnlineBookingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
